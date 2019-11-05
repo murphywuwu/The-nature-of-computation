d284909806d658e853064666adc4201945c1f38c
huangxiulan_cd <huangxiulan_cd@keruyun.com>
2019-11-05 15:28:22 +0800

    🚧 (Regex.rb) 正则表达式：语义- Empty, Literal
    murphywuwu/interview#30

--- chapter-3/Regex.rb
+++ chapter-3/Regex.rb
@@ -1,3 +1,78 @@
+require('set')
+
+class FARule < Struct.new(:state, :character, :next_state)
+  def applies_to?(state, character)
+    self.state == state && self.character == character
+  end
+
+  def follow
+    next_state
+  end
+  
+  def inspect
+    "#<FARule #{state.inspect} -- #{character} --> #{next_state.inspect}"
+  end
+end
+
+class NFARulebook < Struct.new(:rules)
+  def next_states(states, character)
+    states.flat_map { |state| follow_rules_for(state, character) }.to_set
+  end
+
+  def follow_free_moves(states)
+    more_states = next_states(states, nil);
+    # 递归寻找到所有自由状态的自由状态
+    if more_states.subset?(states)
+      states
+    else
+      follow_free_moves(states+more_states) 
+    end
+  end
+
+  def follow_rules_for(state, character)
+    rules_for(state, character).map(&:follow)
+  end
+
+  def rules_for(state, character)
+    # 根据现有状态和接收到的字符串，找到可以转换该状态的所有路线
+    rules.select { |rule| rule.applies_to?(state, character) }
+  end
+end
+
+class NFA < Struct.new(:current_states, :accept_states, :rulebook)
+  def read_character(character)
+    self.current_states = rulebook.next_states(current_states(), character)
+  end
+
+  def read_string(string)
+    string.chars.each do |character|
+      read_character(character)
+    end
+  end
+
+  def current_states
+    rulebook.follow_free_moves(super)
+  end
+
+  def accepting?
+    (current_states & accept_states).any?
+  end
+end
+
+class NFADesign < Struct.new(:start_state, :accept_states, :rulebook)
+  def accepts?(string)
+    to_nfa.tap { |nfa| nfa.read_string(string) }.accepting?
+  end
+
+  def to_nfa
+    # 每调用一次都返回一个新的NFA，并重新设置起始状态
+    # start_state: 起始状态
+    # rulebook: 路线图，每一条规则都是一条路线(某个状态读取某个值变换到另一个状态的路线)
+    # accept_states: 可接受状态
+    NFA.new(Set[start_state], accept_states, rulebook)
+  end
+end
+
 module  Pattern
   # 如果该模式的优先级，比包裹该模式的外部模式的优先级低，则为该模式加括号
   def bracket(outer_precedence)
@@ -12,6 +87,10 @@ module  Pattern
   def inspect
     "/#{self}/"
   end
+  
+  def matches?(string)
+    to_nfa_design.accepts?(string)
+  end
 end
 
 # ''
@@ -25,6 +104,14 @@ class Empty
   def precedence
     3
   end
+
+  def to_nfa_design
+    start_state = Object.new
+    accept_states = [start_state]
+    rulebook = NFARulebook.new([])
+
+    NFADesign.new(start_state, accept_states, rulebook)
+  end
 end
 
 # [a-z|A-Z]
@@ -37,6 +124,14 @@ class Literal < Struct.new(:character)
 
   def precedence
     3
+  end
+
+  def to_nfa_design
+    start_state = Object.new
+    accept_state = Object.new
+    rule = FARule.new(start_state, character, accept_state)
+    rulebook = NFARulebook.new([rule])
+    NFADesign.new(start_state, [accept_state], rulebook)
   end
 end
 
@@ -91,3 +186,14 @@ pattern = Repeat.new(
 )
 
 # /(ab|a)*/
+
+nfa_design = Empty.new.to_nfa_design
+nfa_design.accepts?('') # true
+nfa_design.accepts?('a') # false
+
+nfa_design = Literal.new('a').to_nfa_design
+nfa_design.accepts?('') # false
+nfa_design.accepts?('a') # true
+
+Empty.new().matches?('a') # false
+Literal.new('a').matches?('a') # true
