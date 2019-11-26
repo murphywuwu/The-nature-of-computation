8dadcb259a4dcc9ef5ced5a6b88c496f102ce594
huangxiulan_cd <huangxiulan_cd@keruyun.com>
2019-11-26 16:24:28 +0800

    🚧 (PDA.rb) 规则使用：更新PDA的状态和栈
    murphywuwu/interview#31

--- chapter-4/PDA.rb
+++ chapter-4/PDA.rb
@@ -23,16 +23,31 @@ stack.pop.pop.top # c
 stack.push('x').push('y').top # y
 stack.push('x').push('y').pop.pop # x
 
-# 规则
+# 记录PDA此刻的 状态和栈
 class PDAConfiguration < Struct.new(:state, :stack)
 
 end
 
-class PDARule < Struct.new(:state, :character, :next_state, :pop_character, :push_character)
+# 规则设置
+class PDARule < Struct.new(:state, :character, :next_state, :pop_character, :push_characters)
+
+  # 规则匹配：判断PDA当前状态，栈的内容，输入字符和规则是否匹配
   def applies_to?(configuration, character)
     self.state == configuration.state &&
     self.pop_character = configuration.stack.pop &&
     self.character == character
+  end
+
+  # 规则使用：根据规则更新PAD的状态，栈
+  def follow(configuration)
+    PDAConfiguration.new(next_state, next_stack(configuration))
+  end
+  
+  def next_stack(configuration)
+    popped_statck = configuration.stack.pop
+
+    push_characters.reverse.
+      inject(popped_statck) { |stack, character| stack.push(character) }
   end
 end
 
@@ -41,3 +56,5 @@ rule = PDARule.new(1, '(', 2, '$', ['b', '$'])
 configuration = PDAConfiguration.new(1, Stack.new(['$']))
 rule.applies_to?(configuration, '(') # true
 
+rule.follow(configuration)
+# <struct PDAConfiguration state = 2, stack=#<Stack (b)$>>
