550c7da8e0d6d2ca9ce94171691f7485b7f2515b
huangxiulan_cd <huangxiulan_cd@keruyun.com>
2019-11-26 15:36:22 +0800

    🚧 (PDA.rb) 规则：规则匹配
    murphywuwu/interview#31

--- chapter-4/PDA.rb
+++ chapter-4/PDA.rb
@@ -1,3 +1,4 @@
+# 栈
 class Stack < Struct.new(:contents)
   def push(character)
     Stack.new([character] + contents)
@@ -21,4 +22,22 @@ stack.pop() # a
 stack.pop.pop.top # c
 stack.push('x').push('y').top # y
 stack.push('x').push('y').pop.pop # x
+
+# 规则
+class PDAConfiguration < Struct.new(:state, :stack)
+
+end
+
+class PDARule < Struct.new(:state, :character, :next_state, :pop_character, :push_character)
+  def applies_to?(configuration, character)
+    self.state == configuration.state &&
+    self.pop_character = configuration.stack.pop &&
+    self.character == character
+  end
+end
+
+rule = PDARule.new(1, '(', 2, '$', ['b', '$'])
+
+configuration = PDAConfiguration.new(1, Stack.new(['$']))
+rule.applies_to?(configuration, '(') # true
 
