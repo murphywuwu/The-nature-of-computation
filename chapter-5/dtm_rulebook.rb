6b86ee7d44d3f6456801eef0ee38e9ba08053f36
murphywuwu <murphywuwu@gmail.com>
2019-12-05 17:05:12 +0800

    🚧 (dtm_rulebook.rb) 图灵机：规则
    murphywuwu/interview#32

---
+++ chapter-5/dtm_rulebook.rb
@@ -0,0 +1,34 @@
+require_relative './tm_rule.rb'
+require_relative './tm_configuration.rb'
+require_relative './tape.rb'
+
+class DTMRulebook < Struct.new(:rules)
+  def next_configuration(configuration)
+    rule_for(configuration).follow(configuration)
+  end
+  
+  def rule_for(configuration)
+    rules.detect { |rule| rule.applies_to?(configuration) }
+  end
+end
+
+tape = Tape.new(['1', '0', '1'], '1', [], '_')
+
+
+rulebook = DTMRulebook.new([
+  TMRule.new(1, '1', 1, '0', :left),
+  TMRule.new(1, '0', 2, '1', :right),
+  TMRule.new(1, '_', 2, '1', :right),
+  TMRule.new(2, '0', 2, '0', :right),
+  TMRule.new(2, '1', 2, '1', :right),
+  TMRule.new(2, '_', 3, '_', :left)
+])
+
+configuration = TMConfiguration.new(1, tape)
+#<struct TMConfiguration state=1, tape=#< Tape 101(1)>
+configuration = rulebook.next_configuration(configuration)
+#<struct TMConfiguration state=1, tape=#< Tape 10(1)0>
+configuration = rulebook.next_configuration(configuration)
+#<struct TMConfiguration state=1, tape=#< Tape 1(0)00>
+configuration = rulebook.next_configuration(configuration)
+#<struct TMConfiguration state=2, tape=#< Tape 11(0)0>
